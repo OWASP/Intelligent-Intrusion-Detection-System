5b92b296d1a447d81c8b0bcc8c24d5f72f6170cf
Ashish Malik <44671044+ashish493@users.noreply.github.com>
2020-06-18 12:44:40 +0530

    Update views

--- iids/analyzer/views.py
+++ iids/analyzer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#loading our trained model
+print("Keras model loading.......")
+model = load_model('attack_labe.hdf5') #after training #TODO
+print("Model loaded!!")
+
+    def request():
+        return HttpResponse("<h1>Intelligent Intrusion Detection System</h1>")
+         return HttpResponse("The predicted attacked label is {}".format(label)) #label will be imported after finishing dvevelopment of analyzer
+         
+         
